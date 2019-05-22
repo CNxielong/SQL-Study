d7f48aea2813debda57fff23936f52781dba08b2
“yourCNxielong <youremccnxielong@163.com>
2019-05-22 19:55:37 +0800

    Signed-off-by: “yourCNxielong <“youremccnxielong@163.com>

---
+++ 第十章-SQL入门经典学习.sql
@@ -0,0 +1,11 @@
+--DATE：2019-05-22  Author:谢龙 第十章SQL
+select sum(salary) from employee_pay_tbl;
+
+select sum(salary) from employee_pay_tbl group by salary;
+
+SELECT CITY FROM EMPLOYEE_TBL;
+
+SELECT CITY,COUNT(*) FROM EMPLOYEE_TBL GROUP BY CITY;
+
+SELECT CITY, ZIP,AVG(PAY_RATE),AVG(SALARY) FROM EMPLOYEE_TBL E INNER JOIN EMPLOYEE_PAY_TBL P
+on E.EMP_ID=P.EMP_ID group by city,zip order by city,zip;
