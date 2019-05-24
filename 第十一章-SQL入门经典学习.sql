5bc754cdbfcd6814a537e6d888fe1b369f964cbb
“yourCNxielong <youremccnxielong@163.com>
2019-05-24 17:32:55 +0800

    2019-5-24 提交SQL 第十、十一章

---
+++ 第十一章-SQL入门经典学习.sql
@@ -0,0 +1,6 @@
+---DATE：2019-05-24  Author:xielong Email:cnxielong@gamail.com 第十一章 SQL入门经典
+SELECT CITY,STATE FROM EMPLOYEE_TBL;
+select city || ' '|| state from employee_tbl;
+
+SELECT CITY FROM EMPLOYEE_TBL; 
+SELECT TRANSLATE(CITY,'IND','ABC') FROM EMPLOYEE_TBL;
